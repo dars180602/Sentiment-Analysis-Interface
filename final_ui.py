bc14d8e81e49e879fe56283b91fdff091fa6c431
Cecille Jatulan <109518107+cecjat@users.noreply.github.com>
2024-04-07 14:23:49 -0400

    Add files via upload

---
+++ final_ui.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pickle
+
+# Load the saved model
+with open('nb_bin_model1.pkl', 'rb') as file:
+    loaded_classifier = pickle.load(file)
+
+# Load the saved vectorizer
+with open('tfidf_vect_80mindf.pkl', 'rb') as file:
+    tfidf_vectorizer = pickle.load(file)
+
+# Define the function to make predictions
+def predict_sentiment(review):
+    # Preprocess test data if necessary (ensure it's in the same format as during training)
+
+    # Transform test data using the TF-IDF vectorizer
+    review_tfidf = tfidf_vectorizer.transform([review])
+
+    # Make predictions using the loaded model
+    prediction = loaded_classifier.predict(review_tfidf)
+
+    return prediction
+
+# Create the Streamlit UI
+st.title('Sentiment Analysis App')
+st.write('Enter your review below:')
+
+# Create a text input for user to input review
+review_input = st.text_area('Input your review here:', '')
+
+# Make prediction when button is clicked
+if st.button('Predict Sentiment'):
+    if review_input:
+        prediction = predict_sentiment(review_input)
+        sentiment = 'Positive' if prediction == 1 else 'Negative'
+        st.write(f'The sentiment of the review is: {sentiment}')
+    else:
+        st.write('Please enter a review before predicting.')
